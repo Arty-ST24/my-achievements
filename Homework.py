dc1467e11c0047f36088edaf0415fe862a80125e
Arty-ST24 <ilyas.artamonov2004@gmail.com>
2024-11-26 20:11:08 +0300

    Homework.py

--- Homework.py
+++ Homework.py
@@ -7,8 +7,5 @@ print(2*2+2)
 print(2*(2+2))
 print(2*(2+2) == 2*2+2)
 #4th program
-print(float('123.456'))
-print(123.456 * 10)
-print(int(123.456 * 10))
-
+print(int(float('123.456') * 10) % 10)
 
