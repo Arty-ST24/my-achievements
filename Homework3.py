af9c75f0b9f6171a4f256a2cd6e429f7dd723af6
Arty-ST24 <ilyas.artamonov2004@gmail.com>
2024-11-28 12:39:59 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,5 @@
+homework_completed = 12
+hoursSpent = 1.5
+course_name = "Python"
+timeOnTask = hoursSpent / homework_completed
+print("Курс:",course_name, "," ,"всего задач:", homework_completed, ",", "затрачено часов:", hoursSpent, ",", "среднее время выполнения:", timeOnTask,"часа.")
